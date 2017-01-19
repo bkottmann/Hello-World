585ef57ff66b60b128fd6aa4177e70c04f63a2f6
kottmannucit <brett.kottmann@uc.edu>
2017-01-19 13:52:16 -0500

    main.cpp
    Main routine: passed arg, argv[]; prints results.

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+// Main file for Hello World project
+//
+// Version 1.0
+//
+int main(int argc; char *argv[])
+{
+start = true;
+i = 0;
+while (i <= argc)
+    {
+    do something with argv[i];
+    } // end of while
+
+print("Hello, world!");
+// end of main
