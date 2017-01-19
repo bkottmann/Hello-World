1c134bffc4e88536da3c32c999265c3c8aed6f9a
kottmannucit <brett.kottmann@uc.edu>
2017-01-19 14:00:15 -0500

    Update Main.cpp
    Added another task!

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,16 @@
 // Main file for Hello World project
 //
-// Version 1.0
+// Version 1.0.1
 //
 int main(int argc; char *argv[])
 {
-start = true;
+start = false;
 i = 0;
-while (i <= argc)
+while (i < argc)
     {
     do something with argv[i];
+    then do something with argc
     } // end of while
 
-print("Hello, world!");
+print("Hello, world, again!");
 // end of main
